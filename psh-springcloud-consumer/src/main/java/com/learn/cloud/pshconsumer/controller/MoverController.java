6d33960adedd25a361a5ad59b081e9d7a56a6471
xingpeiyue <xingpeiyue@163.com>
2019-06-29 10:39:34 +0800

    oauth的初步认识

--- psh-springcloud-consumer/src/main/java/com/learn/cloud/pshconsumer/controller/MoverController.java
+++ psh-springcloud-consumer/src/main/java/com/learn/cloud/pshconsumer/controller/MoverController.java
@@ -1,11 +1,22 @@
 package com.learn.cloud.pshconsumer.controller;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import com.learn.cloud.pshconsumer.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class MoverController {
@@ -21,5 +32,31 @@ public class MoverController {
     @GetMapping("/move/{id}")
     public User findById(@PathVariable("id") Long id) {
         return restTemplate.getForObject("http://127.0.0.1:8090/user/" + id, User.class);
+    }
+    @GetMapping("/oauth/redirect")
+    public Object oauth(HttpServletRequest request, HttpServletResponse response){
+        //获取授权码
+        String code = request.getParameter("code");
+
+        HashMap<String,Object> param = new HashMap<>();
+        param.put("client_id","b7ffd6602d645fff3782");
+        param.put("client_secret","e6390498d10fb66dcef232152c3b9b652e623c67");
+        param.put("code",code);
+       String json = HttpRequest.post("https://github.com/login/oauth/access_token")
+                .header("accept","application/json")
+                .form(param)
+                .execute()
+                .body();
+        System.out.println(json);
+        JSONObject result = JSONUtil.parseObj(json);
+        String token = result.getStr("access_token");
+        String type = result.getStr("token_type");
+
+        String user = HttpRequest.get("https://api.github.com/user")
+                .header("accept","application/json")
+                .header("Authorization","token  "+token)
+                .execute()
+                .body();
+        return user;
     }
 }
---
+++ psh-springcloud-oauth/src/main/java/com/learn/cloud/oauth/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.learn.cloud.oauth.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * Created by peiyue.xing on 2019/6/28 19:22
+ *
+ * @version: 1.0
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and().csrf()
+                .disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+}
